56f84cd179d8911a9a32d73c4d21657b6132b922
Lleyton van Sambeeck <lleytonvs1@gmail.com>
2018-10-10 19:12:43 +0800

    Fixed Slider.cs#MouseDown
    Fixed issue with Sliders spazzing around the edges of the main slider body

--- SVNE/SVNE/GUI/Slider.cs
+++ SVNE/SVNE/GUI/Slider.cs
@@ -10,8 +10,10 @@ using SFML.Window;
 
 using SVNE.Core;
 
-namespace SVNE.GUI {
-    class Slider : Clickable {
+namespace SVNE.GUI
+{
+    class Slider : Clickable
+    {
         public RectangleShape bar;
         public RectangleShape handle;
 
@@ -20,7 +22,8 @@ namespace SVNE.GUI {
         public int width;
         public int height;
 
-        public Slider(RectangleShape bar, RectangleShape handle) : base() {
+        public Slider(RectangleShape bar, RectangleShape handle) : base()
+        {
             this.bar = bar;
             this.handle = handle;
 
@@ -29,60 +32,74 @@ namespace SVNE.GUI {
             this.bar.FillColor = new Color(0, 0, 0, 255);
             this.handle.FillColor = new Color(0, 0, 0, 255);
 
-            x = (int) handle.Position.X;
-            y = (int) handle.Position.Y;
-            width = (int) handle.Size.X;
-            height = (int) handle.Size.Y;
+            x = (int)handle.Position.X;
+            y = (int)handle.Position.Y;
+            width = (int)handle.Size.X;
+            height = (int)handle.Size.Y;
         }
 
-        public int GetX {
+        public int GetX
+        {
             get { return x; }
             set { x = value; }
         }
 
-        public int GetY {
+        public int GetY
+        {
             get { return y; }
             set { y = value; }
         }
 
-        public int GetWidth {
+        public int GetWidth
+        {
             get { return width; }
             set { width = value; }
         }
 
-        public int GetHeight {
+        public int GetHeight
+        {
             get { return height; }
             set { height = value; }
         }
 
-        public void Hover() {
-            
+        public void Hover()
+        {
+
         }
 
-        public void MouseDown(RenderWindow window) {
-            if (handle.Position.X > (bar.Position.X + bar.Size.X) - handle.Size.X) {
-                x = (int)((bar.Position.X + bar.Size.X) - handle.Size.X);
-                handle.Position = new Vector2f((bar.Position.X + bar.Size.X) - handle.Size.X, handle.Position.Y);
-            }
-            else if(handle.Position.X < bar.Position.X - 1) {
+        public void MouseDown(RenderWindow window)
+        {
+            int mouseX = Mouse.GetPosition(window).X;
+            int mouseY = Mouse.GetPosition(window).Y;
+
+            x = (int)(mouseX - (handle.Size.X / 2));
+            handle.Position = new Vector2f(mouseX - (handle.Size.X / 2), handle.Position.Y);
+
+            if (handle.Position.X <= bar.Position.X)
+            {
                 x = (int)(bar.Position.X);
                 handle.Position = new Vector2f(bar.Position.X, handle.Position.Y);
             }
-            else {
-                x = (int)(Mouse.GetPosition(window).X - handle.Size.X / 2);
-                handle.Position = new Vector2f(Mouse.GetPosition(window).X - handle.Size.X / 2, handle.Position.Y);
+
+            if (handle.Position.X >= (bar.Position.X + bar.Size.X) - handle.Size.X)
+            {
+                x = (int)((bar.Position.X + bar.Size.X) - handle.Size.X);
+                handle.Position = new Vector2f((bar.Position.X + bar.Size.X) - handle.Size.X, handle.Position.Y);
             }
         }
 
-        public void MouseUp() {
-            
+        public void MouseUp()
+        {
+
         }
 
-        public void Reset() {
-            
+        public void Reset()
+        {
+
         }
 
-        public void Draw(RenderTarget target, RenderStates states) {
+        public void Draw(RenderTarget target, RenderStates states)
+        {
             target.Draw(bar, states);
             target.Draw(handle, states);
         }
